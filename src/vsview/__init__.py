1d28851e80fbf80ac03544af5272fe34c111c7fb
Ichunjo <ichunjo.le.terrible@gmail.com>
2026-03-29 05:20:52 +0200

    api: use lazy imports for api functions

--- src/vsview/__init__.py
+++ src/vsview/__init__.py
@@ -1,3 +1,15 @@
-from .api import catch_output, is_preview, set_output
+from typing import TYPE_CHECKING, Any
 
 __all__ = ["catch_output", "is_preview", "set_output"]
+
+if TYPE_CHECKING:
+    from .api import catch_output, is_preview, set_output
+else:
+
+    def __getattr__(name: str) -> Any:
+        from importlib import import_module
+
+        if name in __all__ and (attr := getattr(import_module("vsview.api"), name, None)):
+            return attr
+
+        raise AttributeError(f"Cannot import {name!r} from 'vsview'")
